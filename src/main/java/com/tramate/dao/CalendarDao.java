2f5ef6c19ae34f030fd7252be6fce65fb3d34da5
FROM SOL프롬 솔 <leesol529@gmail.com>
2019-12-24 11:51:34 +0900

    Calendar 테이블 관련 dto, sql, dao, service, controller 작성

---
+++ src/main/java/com/tramate/dao/CalendarDao.java
@@ -0,0 +1,20 @@
+package com.tramate.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.tramate.dto.CalendarDto;
+
+@Repository
+public class CalendarDao extends SqlSessionDaoSupport{
+	
+	public List<CalendarDto> getSchedule(int num){
+		return getSqlSession().selectOne("calendarGuideSchedule");
+	}
+	
+	public void insertSchedule(CalendarDto dto) {
+		getSqlSession().insert("calendarInsert");
+	}
+}
